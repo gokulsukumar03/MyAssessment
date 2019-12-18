ed6850914a050a998f82d30177bf556ff9df5dac
sukumar031@gmail.com <03#Myproject>
2019-12-18 16:48:01 +0530

    Implemented espresso

--- app/src/androidTest/java/com/sukumar/tetrasoft/MostPopularUITestCase.java
+++ app/src/androidTest/java/com/sukumar/tetrasoft/MostPopularUITestCase.java
@@ -1,6 +1,7 @@
 package com.sukumar.tetrasoft;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import com.sukumar.tetrasoft.module.mostPopular.ui.MostPopularActivity;
@@ -15,13 +16,15 @@ import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class MostPopularUITestCase {
@@ -66,19 +69,26 @@ public class MostPopularUITestCase {
     public void MOST_POPULAR_ACTIVITY_FETCH_DATA(){
         MockResponse response = new MockResponse().setBody(mockResponse).setBodyDelay(5, TimeUnit.SECONDS);
         onView(withId(R.id.mProgressBar)).check(matches(isDisplayed()));
+        onView(withId(R.id.errorTv)).check(matches(not(isDisplayed())));
         mockWebServer.enqueue(response);
-        /*onView(withId(R.id.mMostPopularRecyclerView))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));*/
+
     }
 
-/*
     @Test
-    public void MOST_POPULAR_ACTIVITY_FETCH_DATA_FAILURE(){
+    public void MOST_POPULAR_ACTIVITY_FETCH_DATA_RECYCLER_VIEW_SCROLL() throws InterruptedException {
+        Thread.sleep(2000);
+        onView(withId(R.id.mMostPopularRecyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+    }
+
+    @Test
+    public void MOST_POPULAR_ACTIVITY_FETCH_DATA_FAILURE() {
         MockResponse response = new MockResponse().setBodyDelay(5, TimeUnit.SECONDS).setResponseCode(HttpURLConnection.HTTP_INTERNAL_ERROR);
         mockWebServer.enqueue(response);
         onView(withId(R.id.errorTv)).check(matches(withText("Error loading")));
         onView(withId(R.id.errorTv)).check(matches(isDisplayed()));
-    }*/
+        onView(withId(R.id.mProgressBar)).check(matches(not(isDisplayed())));
+    }
 
     @After
     public void  tearDown() throws IOException {
